ee9aba57a3eb3c2a0ef3a04685fbf0d3a6deb1fe
owelray <57111549+owelray@users.noreply.github.com>
2020-06-26 11:47:54 +0300

    test

--- config/settings.py
+++ config/settings.py
@@ -82,13 +82,6 @@ CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
